42797383ac63f3d22ccba164eb48701a054612a8
PointerToNextPole <pointertonextpole@gmail.com>
2021-01-08 14:03:55 +0800

    add some needed  package or plugins

--- datav-report/.eslintrc.js
+++ datav-report/.eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // indent锁进
+    indent: 'off'
   }
 }
--- datav-report/src/main.js
+++ datav-report/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import ECharts from 'echarts'
 
 Vue.config.productionTip = false
+Vue.prototype.$echarts = ECharts //每一个组件在引用ECharts时，便不需要import，使用$echarts即可
 
 new Vue({
   router,

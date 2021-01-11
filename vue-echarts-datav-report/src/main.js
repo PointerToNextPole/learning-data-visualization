cbbd368ed621e96bd14edb1811879b0fe6a9144f
PointerToNextPole <pointertonextpole@gmail.com>
2021-01-11 13:24:51 +0800

    start data-report with vue-echarts

---
+++ vue-echarts-datav-report/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import ECharts from 'echarts'
+
+Vue.config.productionTip = false
+Vue.prototype.$echarts = ECharts // 每一个组件在引用ECharts时，便不需要import，使用$echarts即可
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
